ae433b5b56c87fe0c8e3086de55e818dea3db60d
Olha Smyk <obsmyk202001@gmail.com>
2025-03-01 16:35:15 +0100

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function getElementWidth(content, padding, border) {
+  return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+}
+
+
+
+/*const contentWidth = Number.parseFloat(content);
+  const paddingWidth = Number.parseFloat(padding) * 2;
+  const borderWidth = Number.parseFloat(border) * 2;
+  return contentWidth + paddingWidth + borderWidth;*/
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
