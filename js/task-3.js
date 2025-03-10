c8112f06b057743397ba9052f3add6b0c8622f6b
Olya <obsmyk202001@gmail.com>
2025-03-10 19:59:41 +0100

    add all tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,20 @@
-function getElementWidth(content, padding, border) {
-  return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+function checkForSpam(message) {
+    const messageLow = message.toLowerCase();
+
+  if (messageLow.includes('spam') || messageLow.includes('sale')) {
+    return true;
+    }
+  else {
+      return false;
+    }
+  
 }
 
 
-
-/*const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding) * 2;
-  const borderWidth = Number.parseFloat(border) * 2;
-  return contentWidth + paddingWidth + borderWidth;*/
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
